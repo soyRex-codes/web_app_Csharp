386f2704e6ee836ed9698217c1125272bf40648b
Rajkumar Kushwaha <rajkushwaha0213@gmail.com>
2026-06-17 15:06:11 -0500

    reconnecting to my repo - after a minor error refresh (#13)

--- web_app_Csharp/obj/Debug/net10.0/web_app_Csharp.AssemblyInfo.cs
+++ web_app_Csharp/obj/Debug/net10.0/web_app_Csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_app_Csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88b610a752ec86ca21d97efb8745617135a1c0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92935a542aae59fb03a394110086799e68d261fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_app_Csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_app_Csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
