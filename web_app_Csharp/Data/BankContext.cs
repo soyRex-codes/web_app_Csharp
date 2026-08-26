07f8821888707233cc9c8834a3e4124589bad382
Rajkumar Kushwaha <rajkushwaha0213@gmail.com>
2026-08-26 16:17:21 -0500

    feat: add Identity persistence foundation

--- web_app_Csharp/Data/BankContext.cs
+++ web_app_Csharp/Data/BankContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using web_app_Csharp.Features.Accounts;
+using web_app_Csharp.Features.Identity;
 
 namespace web_app_Csharp.Data;
 
-public sealed class BankContext(DbContextOptions<BankContext> options) : DbContext(options)
+public sealed class BankContext(DbContextOptions<BankContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
     public DbSet<BankAccount> Accounts => Set<BankAccount>();
 
--- web_app_Csharp/Program.cs
+++ web_app_Csharp/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using System.Text.Json.Serialization;
 using web_app_Csharp.Data;
 using web_app_Csharp.Features.Accounts;
+using web_app_Csharp.Features.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,19 @@ builder.Services.AddDbContext<BankContext>(options =>
     options.UseSqlServer(
         connectionString,
         sqlServerOptions => sqlServerOptions.EnableRetryOnFailure()));
+builder.Services
+    .AddIdentityCore<ApplicationUser>(options =>
+    {
+        options.User.RequireUniqueEmail = true;
+        options.Password.RequiredLength = 12;
+        options.Password.RequireDigit = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<BankContext>()
+    .AddSignInManager();
 builder.Services.AddOpenApi();
 builder.Services.ConfigureHttpJsonOptions(options =>
     options.SerializerOptions.Converters.Add(new JsonStringEnumConverter()));
